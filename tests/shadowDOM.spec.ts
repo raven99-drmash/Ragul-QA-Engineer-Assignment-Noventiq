0597ecd930e03241330b6675ef63b5817e67a564
raven99-drmash <ragulragul007.rr@gmail.com>
2026-01-08 13:55:55 +0530

    Shadow test run

--- tests/shadowDOM.spec.ts
+++ tests/shadowDOM.spec.ts
@@ -1,7 +1,15 @@
 import { test } from '@playwright/test';
+import * as XL from 'xlsx';
 
-
-test('tc01', async ({ page }) => {
+test.skip('tc01', async ({ page }) => {
     await page.goto('https://practice.expandtesting.com/shadowdom');
     console.log(await page.locator(`#my-btn[type='button']`).first().innerText());
-}) 
+})
+
+test('tc02', async ({ page }) => {
+    const wbk: XL.WorkBook = XL.readFile('test-data/TestCasesDoc.xlsx');
+    const data = XL.utils.sheet_to_json(wbk.Sheets[wbk.SheetNames[0]]); //converts sheet data to json
+
+
+    console.log(data);
+})
